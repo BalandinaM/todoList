bf6b9659bdbd11f6fe0b312217cbbc7eaff54482
BalandinaM <marina.balandina.89@mail.ru>
2025-08-30 15:30:15 +0500

    4 lesson, init project, render task list

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const tasks = [
+    { id: 1, title: "Купить продукты на неделю", isDone: false },
+    { id: 2, title: "Полить цветы", isDone: true },
+    { id: 3, title: "Сходить на тренировку", isDone: false },
+  ];
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Список дел</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <div>{task.title}</div>
+            <input type="checkbox" checked={task.isDone}/>
+          </li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
